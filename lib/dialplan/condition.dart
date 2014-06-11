118119e6a501570cd8db71fe03bdfc570b25c195
Thomas Pedersen <tp@adaheads.com>
2014-06-11 14:27:42 +0200

    Forgot to deserialize Date object.

--- lib/dialplan/condition.dart
+++ lib/dialplan/condition.dart
@@ -6,9 +6,11 @@ class Condition implements JsonSerializable {
 
   factory Condition.fromJson(Map json) {
     switch (json['condition'] as String) {
+      case 'date':
+        return new Date.fromJson(json);
+
       case 'time':
         return new Time.fromJson(json);
-        break;
 
       default:
         throw ('Unknown condition "${json['condition']}"');
